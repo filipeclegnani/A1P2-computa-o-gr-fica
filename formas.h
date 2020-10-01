3a42117e033f6645f032ee75f86a2d6df8202432
blackmetal11 <39375254+blackmetal11@users.noreply.github.com>
2020-10-01 20:39:24 -0300

    Ajustes na construção do projeto

---
+++ formas.h
@@ -0,0 +1,9 @@
+#ifndef FORMAS_H
+#define FORMAS_H
+
+// Colocar o código aqui
+
+
+
+
+#endif // deve ser a ultima linha do arquivo
--- janela.h
+++ janela.h
@@ -1,1 +1,9 @@
+#ifndef JANELA_H
+#define JANELA_H
 
+// Colocar o código aqui
+
+
+
+
+#endif // deve ser a ultima linha do arquivo
